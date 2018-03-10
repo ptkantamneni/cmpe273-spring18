fd98021510a184b8be2d1d5d9dcd8e24eb1484b8
Phani Teja Kantamneni <teja.kantamneni@outlook.com>
2018-03-09 19:12:43 -0800

    Updated server.py

--- Labs/Lab3/server.py
+++ Labs/Lab3/server.py
@@ -10,32 +10,16 @@ sock = context.socket(zmq.PUB)
 sock1 = context.socket(zmq.REP)
 sock.bind("tcp://127.0.0.1:5680")
 sock1.bind("tcp://127.0.0.1:5677")
-
-mess = ""
-prev_mess = ""
-
-def in_thread():
-    global mess, prev_mess
-    mess = sock1.recv_string()
-    sock1.send_string("")
-    prev_mess = mess
+try:
     while True:
         mess = sock1.recv_string()
         sock1.send_string("")
-    
-def out_thread():
-    global mess, prev_mess
-    while True:
-        if prev_mess != mess:
-            messages = mess.split(" ",1)
+        messages = mess.split(" ",1)
+        try:
             sock.send_string(messages[0]+" : "+messages[1])
-            prev_mess = mess
-
-thread1 = threading.Thread(target=in_thread)
-thread2 = threading.Thread(target=out_thread)
-
-thread1.start()
-thread2.start()
-
-
-
+        except IndexError:
+            print("Invalid arguments")
+            exit(0)
+except KeyboardInterrupt:
+    print("Disconnected!")
+    exit(0)
